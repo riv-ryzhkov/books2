a49d98e27c0aa6f061a980e6e5ff3529f7ca5e61
riv-ryzhkov <riv.ryzhkov@gmail.com>
2022-04-29 19:42:33 +0300

    My Django Books2 app, first commit

---
+++ main/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
+
+
+def index(request):
+    books = Book.objects.all()
+    # books = Book.objects.order_by('-id')
+    # books = Book.objects.order_by('-id')[:3]
+    # books = Book.objects.filter(author='ГОСТ1')
+    # books = Book.objects.filter(id__gt=2)
+    # books = Book.objects.filter(id__lt=5)
+    return render(request, 'main/index.html', {'title': 'Головна сторінка', 'books': books})
+    # return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = BookForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html', context)
+    # return render(request, 'main/create.html')
+
+# Create your views here.
