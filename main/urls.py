a71f3c6d3ef6b7116f3dde0961f004e366b74b4d
riv-ryzhkov <riv.ryzhkov@gmail.com>
2022-07-12 18:57:49 +0300

    Add API commit

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import BookAPIView
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
+    path('api/v1/booklist/', BookAPIView.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, redirect
+from rest_framework import generics
+
 from .models import Book
 from .forms import BookForm
+from .serializers import BookSerializers
+
+
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
+
+
+
+
 
 
 def index(request):
+
     books = Book.objects.all()
     # books = Book.objects.order_by('-id')
     # books = Book.objects.order_by('-id')[:3]
@@ -12,6 +25,33 @@ def index(request):
     # books = Book.objects.filter(id__lt=5)
     return render(request, 'main/index.html', {'title': 'Головна сторінка', 'books': books})
     # return render(request, 'main/index.html')
+
+def index(request):
+    # books = [
+    #     {
+    #         "author": 'Pushkin',
+    #         "id": '88',
+    #         "title": 'Ruslan',
+    #         "text": 'gjkhkjhlkj',
+    #         "count": 8
+    #     },
+    #     {
+    #         "author": 'Pushkin2',
+    #         "id": '89',
+    #         "title": 'Ruslan2',
+    #         "text": 'gjkhkjhlkj222',
+    #         "count": 10
+    #     },
+    # ]
+    books = Book.objects.all()
+    # books = Book.objects.order_by('-id')
+    # books = Book.objects.order_by('-id')[:3]
+    # books = Book.objects.filter(author='ГОСТ1')
+    # books = Book.objects.filter(id__gt=2)
+    # books = Book.objects.filter(id__lt=5)
+    return render(request, 'main/index.html', {'title': 'Головна сторінка', 'books': books})
+    # return render(request, 'main/index.html')
+
 
 def about(request):
     return render(request, 'main/about.html')
